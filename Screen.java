a04cb47d96141221aadaddcaa5771a5379f196ba
imjonsnow <dwo6030@uncw.edu>
2015-05-15 20:54:06 -0400

    Update Screen.java

--- Screen.java
+++ Screen.java
@@ -1,12 +1,17 @@
 import javax.swing.JPanel;
+
 import java.util.Scanner;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.Timer;
 
@@ -14,10 +19,16 @@ public class Screen extends JPanel implements KeyListener{
 	Timer timer;
 	Scanner scan;
 	ArrayList screenObjects;
-	public static int screenWidth = 300;
-	public static int screenHeight = 300;
+	public static int screenWidth = 400;
+	public static int screenHeight = 400;
 	//initialize movement vectors 
 	public static Vector laserVector = new Vector(0,0);
+	
+	//initialize baffle stuff
+	Point bafflePoint;
+	Rectangle baffleArea;
+	Image baffleImage;
+	double baffleAngle; //note: no baffle image
 	
 	public Screen()
 	{
@@ -29,12 +40,28 @@ public class Screen extends JPanel implements KeyListener{
 		screenObjects = new ArrayList<ScreenObject>();
 		timer = new javax.swing.Timer(25, new TimerListener());
 		timer.start();
-		// addBaffles();
+		// addBaffle();
 
 	}
 	
-	public void addBaffles(){
-		
+	public void addBaffle(){
+		Random rand = new Random();
+		int x = rand.nextInt(10);
+		int y = rand.nextInt(10);
+		int determine = rand.nextInt(1);
+		if (determine == 1){
+			baffleAngle = 45;
+		}
+		else{
+			baffleAngle = 135;
+		}
+		//create the baffle based on those points
+		int baffleX = (screenWidth/x) - 20;
+		int baffleY = (screenHeight/y) - 20;
+		bafflePoint = new Point(baffleX, baffleY);
+		baffleArea = new Rectangle(20, 1);
+		Baffle baffle = new Baffle(bafflePoint, baffleArea, baffleImage, baffleAngle);
+		//note: no actual baffle image
 	}
 	
 	public void shoot(int locat){
@@ -81,6 +108,7 @@ public void keyTyped(KeyEvent event) {
 		int location = scan.nextInt();
 		while (location > 39){
 			System.out.println("Please enter a valid location");
+			location = scan.nextInt();
 		}
 		shoot(location);
 		break;
